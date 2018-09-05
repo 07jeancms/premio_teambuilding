1c80b236709d441fbedaffa82cae47ffe7334804
Jean Carlo Mata <jean.cms@hotmail.es>
2018-09-04 21:36:43 -0600

    Modifying Create and List options

--- CRUD/products/premio-products/products-create.php
+++ CRUD/products/premio-products/products-create.php
@@ -13,25 +13,22 @@ function premio_products_create() {
                 array('premio_product' => $premio_product, 'name' => $name), //data
                 array('%s', '%s') //data format			
         );
-        $message.="School inserted";
+        $message.="Product Inserted";
     }
     ?>
-    <link type="text/css" href="<?php echo WP_PLUGIN_URL; ?>/sinetiks-schools/style-admin.css" rel="stylesheet" />
+    <link type="text/css" href="<?php echo WP_PLUGIN_URL; ?>/premio-products/style-admin.css" rel="stylesheet" />
     <div class="wrap">
-        <h2>Add New School</h2>
+        <h2>Add New Product</h2>
         <?php if (isset($message)): ?><div class="updated"><p><?php echo $message; ?></p></div><?php endif; ?>
         <form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
-            <p>Three capital letters for the ID</p>
+
             <table class='wp-list-table widefat fixed'>
                 <tr>
-                    <th class="ss-th-width">ID</th>
-                    <td><input type="text" name="id" value="<?php echo $id; ?>" class="ss-field-width" /></td>
-                </tr>
-                <tr>
-                    <th class="ss-th-width">School</th>
+                    <th class="ss-th-width">Product</th>
                     <td><input type="text" name="name" value="<?php echo $name; ?>" class="ss-field-width" /></td>
                 </tr>
             </table>
+            
             <input type='submit' name="insert" value='Save' class='button'>
         </form>
     </div>
