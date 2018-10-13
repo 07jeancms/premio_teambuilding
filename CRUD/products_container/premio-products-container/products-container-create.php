e3d843d074d26da75ca703057e76b483c850fd4b
Jean Carlo Mata <jean.cms@hotmail.es>
2018-10-13 16:29:40 -0600

    Adding option to add products to product_containers

--- CRUD/products_container/premio-products-container/products-container-create.php
+++ CRUD/products_container/premio-products-container/products-container-create.php
@@ -1,10 +1,12 @@
 <?php
 
 function premio_products_container_create() {
+    global $wpdb;
+    $product_table = $wpdb->prefix . "premio_product";
     $name = $_POST["name"];
+    $products = $wpdb->get_results("SELECT * from $product_table");
     //insert
     if (isset($_POST['insert'])) {
-        global $wpdb;
         $table_name = $wpdb->prefix . "premio_product_container";
 
         $wpdb->insert(
@@ -12,6 +14,25 @@ function premio_products_container_create() {
                 array('name' => $name), //data
                 array('%s') //data format			
         );
+
+        if(!empty($_POST['checkbox'])) {
+            foreach($_POST["checkbox"] as $v) {
+                $product_id_to_int = (int)$v;
+
+                $last_inserted_product_container_id = $wpdb->get_row($wpdb->prepare(
+                    "SELECT product_container_id as last_inserted_product_container_id FROM wp_premio_product_container ORDER BY product_container_id DESC LIMIT 1"
+                ));
+
+                $wpdb->insert(
+                    $wpdb->prefix.'products_by_container', 
+                    array(
+                        'id_products_by_container' => NULL,
+                        'product_product_id_fk' => $product_id_to_int, 
+                        'product_container_id_fk' => $last_inserted_product_container_id->last_inserted_product_container_id)
+                );
+            }
+        }
+
         $message.="Container inserted";
     }
     ?>
@@ -24,6 +45,14 @@ function premio_products_container_create() {
                 <tr>
                     <th class="ss-th-width">Container name</th>
                     <td><input type="text" name="name" value="<?php echo $name; ?>" class="ss-field-width" /></td>
+                </tr>
+                <tr>
+                    <th class="ss-th-width">Products</th>
+                    <td>
+                        <?php foreach ($products as $product) { ?>
+                            <input name="checkbox[]" type="checkbox" id="checkbox[]" value="<?php echo $product->product_id; ?>"> <?php echo $product->name; ?> <br>
+                        <?php } ?>
+                    </td>
                 </tr>
             </table>
             <input type='submit' name="insert" value='Save' class='button'>
